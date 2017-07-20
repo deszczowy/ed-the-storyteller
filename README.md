c7e2e4c415e7ef69a9adb8703eb2525bd726ac8b
deszczowy <krystian.szklarek@op.pl>
2017-07-20 20:00:34 +0200

    initial project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ed, The Storyteller
+
+Simple storyboard app written in Angular2, wrapped in Electron environment.
+
+## Build
+
+Use `npm run go` to build and run.
---
+++ main.js
@@ -0,0 +1,61 @@
+const {app, BrowserWindow} = require('electron')
+require('electron-reload')(__dirname);
+
+const path = require('path')
+const url = require('url')
+
+// put console logs to a file
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+//
+
+require('dotenv').config();
+
+let win = null;
+
+app.on('ready', function () {
+
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({width: 1000, height: 600});
+
+  // Specify entry point
+  if (process.env.PACKAGE === 'true'){
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'dist/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  } else {
+    win.loadURL(process.env.HOST);
+    win.webContents.openDevTools();
+  }
+
+  // Show dev tools
+  // Remove this line before distributing
+  win.webContents.openDevTools()
+
+  // Remove window once app is closed
+  win.on('closed', function () {
+    win = null;
+  });
+
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
